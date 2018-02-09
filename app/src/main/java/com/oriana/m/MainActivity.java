d28334115e677f1549d3c97a958a3c0d91a9d8a7
Jim <jamal.radi.90@gmail.com>
2018-02-09 16:36:09 +0000

    m

---
+++ app/src/main/java/com/oriana/m/MainActivity.java
@@ -0,0 +1,15 @@
+package com.oriana.m;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
